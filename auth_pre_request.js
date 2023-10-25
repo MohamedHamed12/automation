0de2fec17c9d3291b0cfe79a9ba218fb30a13f45
Mohamed Hamed <90472426+MohamedHamed12@users.noreply.github.com>
2023-10-25 20:39:19 +0300

    Create auth_pre_request.js

---
+++ auth_pre_request.js
@@ -0,0 +1,46 @@
+const login_url=pm.variables.get("login_url");
+const email=pm.variables.get("email");
+const password=pm.variables.get("password");
+
+const echoPostRequest = {
+
+  url: login_url,
+  method: 'POST',
+  header: 'Content-Type:application/json',
+  body: {
+    mode: 'application/json',
+    raw: JSON.stringify(
+        {
+        	'email':email,
+            'password':password
+        })
+  }
+};
+
+var getToken = true;
+
+if (!pm.environment.get('accessTokenExpiry') || 
+    !pm.environment.get('currentAccessToken')) {
+    console.log('Token or expiry date are missing')
+} else if (pm.environment.get('accessTokenExpiry') <= (new Date()).getTime()) {
+    console.log('Token is expired')
+} else {
+    getToken = false;
+    console.log('Token and expiry date are all good');
+}
+
+if (getToken === true) {
+    pm.sendRequest(echoPostRequest, function (err, res) {
+    console.log(err ? err : res.json());
+        if (err === null) {
+            console.log('Saving the token and expiry date')
+            var responseJson = res.json();
+            pm.environment.set('currentAccessToken', responseJson.access)
+    
+            var expiryDate = new Date();
+            expiryDate.setSeconds(expiryDate.getSeconds() + responseJson.expires_in);
+            pm.environment.set('accessTokenExpiry', expiryDate.getTime());
+        }
+    });
+}
+
